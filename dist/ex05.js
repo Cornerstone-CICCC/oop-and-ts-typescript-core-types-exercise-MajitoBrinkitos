c0c73d456bffbecfcf6f120dd243e18ffac5cceb
Majito Brinkitos <mcvef2013@gmail.com>
2025-02-05 12:03:52 -0800

    ex01, ex02, ex03, ex04, ex05

--- dist/ex05.js
+++ dist/ex05.js
@@ -2,6 +2,12 @@
 // Exercise: Create a function `setTheme` that accepts a parameter with the literal type 'dark' or 'light'.
 // The function should return a string saying "Theme set to [theme]".
 function setTheme(theme) {
+    if (theme === 'dark') {
+        return "Theme set to dark";
+    }
+    else {
+        return "Theme set to light";
+    }
 }
 console.log(setTheme('dark'));
 // Expected output: "Theme set to dark"
--- src/ex05.ts
+++ src/ex05.ts
@@ -1,8 +1,12 @@
 // Exercise: Create a function `setTheme` that accepts a parameter with the literal type 'dark' or 'light'.
 // The function should return a string saying "Theme set to [theme]".
 
-function setTheme(theme) {
-  
+function setTheme(theme: any) {
+  if(theme === 'dark'){
+    return "Theme set to dark";
+  } else {
+    return "Theme set to light";
+  }
 }
 
 console.log(setTheme('dark'));
