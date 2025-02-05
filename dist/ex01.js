a92136ca25390b8eee36274304c187762cb13f71
Majito Brinkitos <mcvef2013@gmail.com>
2025-02-05 10:00:45 -0800

    ex01

--- dist/ex01.js
+++ dist/ex01.js
@@ -1,22 +1,15 @@
+"use strict";
 // Exercise: Define a type `Person` with the following properties:
 // - `firstName` (string)
 // - `lastName` (string)
 // - `age` (number)
 // Then, create a function `greetPerson` that accepts a `Person` object and returns a greeting string: 
 // "Hello, [firstName] [lastName]! You are [age] years old."
-
-type Person = {
-  firstName: string;
-  lastName: string;
-  age: number;
-};
-
-function greetPerson(person: string): Person{
-  const Person = [
-    `Hello, ${person.firstName} ${person.lastName}! You are ${person.age} years`
-  ]
-  return Person
+function greetPerson(person) {
+    var Person = [
+        "Hello, ".concat(person.firstName, " ").concat(person.lastName, "! You are ").concat(person.age, " years")
+    ];
+    return Person;
 }
-
 console.log(greetPerson({ firstName: 'John', lastName: 'Doe', age: 30 }));
 // Expected output: "Hello, John Doe! You are 30 years old."
---
+++ dist/ex06.js
@@ -0,0 +1,7 @@
+"use strict";
+// Exercise: Define a type alias `Rectangle` with two properties `width` and `height` (both numbers).
+// Then, create a function `calculateArea` that takes a `Rectangle` and returns its area.
+function calculateArea(rect) {
+}
+console.log(calculateArea({ width: 10, height: 5 }));
+// Expected output: 50
