6204fab8cccfff246a4f9e1bf27956b8f35902b4
Majito Brinkitos <mcvef2013@gmail.com>
2025-02-05 12:31:39 -0800

    ex01, ex02, ex03, ex04, ex05, ex06

--- dist/ex06.js
+++ dist/ex06.js
@@ -2,6 +2,7 @@
 // Exercise: Define a type alias `Rectangle` with two properties `width` and `height` (both numbers).
 // Then, create a function `calculateArea` that takes a `Rectangle` and returns its area.
 function calculateArea(rect) {
+    return rect.width * rect.height;
 }
 console.log(calculateArea({ width: 10, height: 5 }));
 // Expected output: 50
