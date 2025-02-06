c8de6f1b2c1d3be4984b8a225e2ab805e470023c
Majito Brinkitos <mcvef2013@gmail.com>
2025-02-05 19:37:53 -0800

    all exercises

--- src/ex07.ts
+++ src/ex07.ts
@@ -1,5 +1,17 @@
 // Exercise: Write a function `fail` that takes a string message and throws an error with that message.
 // The function should have a `never` return type because it never successfully returns.
 
-console.log(fail("Something went wrong"));
+function fail(message: string, code: number) : never {
+    throw { message: message, errorCode: code };
+}
+
+try {
+    fail("Uncaught Error: Something went wrong");
+} catch (error) {
+    console.log(error.message);
+    console.log(error.errorCode);
+}
+
+
+
 // Expected output: Uncaught Error: Something went wrong
